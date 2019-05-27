dd83ed0de07065754800bb98a84339cf9a7577b0
melikeyilmaz <melike.yilmaz@ogr.deu.edu.tr>
2019-05-27 19:56:17 +0300

    kullanici

--- Otelprojesii/Otelprojesii/Kullanici.cs
+++ Otelprojesii/Otelprojesii/Kullanici.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace Otelprojesii
 {
-    class Kullanici
+    abstract class Kullanici
     {
+        private int kullaniciId;
+        private string kullaniciAdi;
+        private string sifre;
+        private int telefon;
+        private string email;
+
+        public int KullaniciId
+        {
+            get { return kullaniciId; }
+            set { kullaniciId = value; }
+        }
+        public string KullaniciAdi
+        {
+            get { return kullaniciAdi; }
+            set { kullaniciAdi = value; }
+        }
+        public string Sifre
+        {
+            get { return sifre; }
+            set { sifre = value; }
+        }
+        public int Telefon
+        {
+            get { return telefon; }
+            set { telefon = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+
+        }
+
+        public Kullanici(int Id, string Ad, string sifre, int telefon, string email)
+        {
+            KullaniciId = Id;
+            KullaniciAdi = Ad;
+            Sifre = sifre;
+            Telefon = telefon;
+            Email = email;
+        }
+
+
+
+
     }
 }
