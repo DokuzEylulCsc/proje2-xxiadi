9eda70908ab4c7c275e9314740bb67bc7262d7a1
melikeyilmaz <melike.yilmaz@ogr.deu.edu.tr>
2019-05-27 18:39:17 +0300

    ll

--- Otelprojesii/Otelprojesii/OtelSecmesi.Designer.cs
+++ Otelprojesii/Otelprojesii/OtelSecmesi.Designer.cs
@@ -52,16 +52,16 @@
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(12, 418);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(9, 340);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(130, 24);
+            this.label5.Size = new System.Drawing.Size(103, 18);
             this.label5.TabIndex = 47;
             this.label5.Text = "Otel Özellikleri";
             // 
@@ -69,26 +69,27 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(12, 89);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(9, 72);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(146, 24);
+            this.label4.Size = new System.Drawing.Size(119, 18);
             this.label4.TabIndex = 46;
             this.label4.Text = "Otel Özellikleri";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(393, 27);
+            this.dateTimePicker1.Location = new System.Drawing.Point(295, 22);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(151, 20);
             this.dateTimePicker1.TabIndex = 45;
             // 
             // checkBox9
             // 
             this.checkBox9.AutoSize = true;
-            this.checkBox9.Location = new System.Drawing.Point(357, 351);
+            this.checkBox9.Location = new System.Drawing.Point(268, 285);
+            this.checkBox9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox9.Name = "checkBox9";
-            this.checkBox9.Size = new System.Drawing.Size(108, 21);
+            this.checkBox9.Size = new System.Drawing.Size(84, 17);
             this.checkBox9.TabIndex = 44;
             this.checkBox9.Text = "Spor Salonu";
             this.checkBox9.UseVisualStyleBackColor = true;
@@ -96,9 +97,10 @@
             // checkBox8
             // 
             this.checkBox8.AutoSize = true;
-            this.checkBox8.Location = new System.Drawing.Point(181, 351);
+            this.checkBox8.Location = new System.Drawing.Point(136, 285);
+            this.checkBox8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox8.Name = "checkBox8";
-            this.checkBox8.Size = new System.Drawing.Size(55, 21);
+            this.checkBox8.Size = new System.Drawing.Size(45, 17);
             this.checkBox8.TabIndex = 43;
             this.checkBox8.Text = "Spa";
             this.checkBox8.UseVisualStyleBackColor = true;
@@ -106,9 +108,10 @@
             // checkBox7
             // 
             this.checkBox7.AutoSize = true;
-            this.checkBox7.Location = new System.Drawing.Point(16, 351);
+            this.checkBox7.Location = new System.Drawing.Point(12, 285);
+            this.checkBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox7.Name = "checkBox7";
-            this.checkBox7.Size = new System.Drawing.Size(70, 21);
+            this.checkBox7.Size = new System.Drawing.Size(57, 17);
             this.checkBox7.TabIndex = 42;
             this.checkBox7.Text = "Havuz";
             this.checkBox7.UseVisualStyleBackColor = true;
@@ -117,10 +120,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(12, 241);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(9, 196);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(126, 24);
+            this.label3.Size = new System.Drawing.Size(101, 18);
             this.label3.TabIndex = 41;
             this.label3.Text = "Yatak Seciniz:";
             // 
@@ -128,10 +130,9 @@
             // 
             this.radioButton4.AutoSize = true;
             this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton4.Location = new System.Drawing.Point(360, 282);
-            this.radioButton4.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton4.Location = new System.Drawing.Point(270, 229);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(115, 28);
+            this.radioButton4.Size = new System.Drawing.Size(95, 22);
             this.radioButton4.TabIndex = 40;
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "İkiz Yatakli";
@@ -141,10 +142,9 @@
             // 
             this.radioButton5.AutoSize = true;
             this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton5.Location = new System.Drawing.Point(181, 282);
-            this.radioButton5.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton5.Location = new System.Drawing.Point(136, 229);
             this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(107, 28);
+            this.radioButton5.Size = new System.Drawing.Size(90, 22);
             this.radioButton5.TabIndex = 39;
             this.radioButton5.TabStop = true;
             this.radioButton5.Text = "Cift Kisilik";
@@ -154,10 +154,9 @@
             // 
             this.radioButton6.AutoSize = true;
             this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton6.Location = new System.Drawing.Point(15, 282);
-            this.radioButton6.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton6.Location = new System.Drawing.Point(11, 229);
             this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(114, 28);
+            this.radioButton6.Size = new System.Drawing.Size(93, 22);
             this.radioButton6.TabIndex = 38;
             this.radioButton6.TabStop = true;
             this.radioButton6.Text = "Tek Kisilik";
@@ -166,18 +165,17 @@
             // BulunanOteller
             // 
             this.BulunanOteller.FormattingEnabled = true;
-            this.BulunanOteller.ItemHeight = 16;
-            this.BulunanOteller.Location = new System.Drawing.Point(939, 27);
+            this.BulunanOteller.Location = new System.Drawing.Point(704, 22);
+            this.BulunanOteller.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BulunanOteller.Name = "BulunanOteller";
-            this.BulunanOteller.Size = new System.Drawing.Size(262, 484);
+            this.BulunanOteller.Size = new System.Drawing.Size(198, 394);
             this.BulunanOteller.TabIndex = 37;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(763, 26);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(572, 21);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 48);
+            this.button1.Size = new System.Drawing.Size(81, 39);
             this.button1.TabIndex = 36;
             this.button1.Text = "ARA  🔍";
             this.button1.UseVisualStyleBackColor = true;
@@ -186,10 +184,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(12, 163);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(9, 132);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 24);
+            this.label2.Size = new System.Drawing.Size(98, 18);
             this.label2.TabIndex = 35;
             this.label2.Text = "Yıldız Seçiniz:";
             // 
@@ -197,10 +194,9 @@
             // 
             this.radioButton3.AutoSize = true;
             this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton3.Location = new System.Drawing.Point(361, 191);
-            this.radioButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton3.Location = new System.Drawing.Point(271, 155);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(96, 28);
+            this.radioButton3.Size = new System.Drawing.Size(81, 22);
             this.radioButton3.TabIndex = 34;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "★★★★★";
@@ -210,10 +206,9 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton2.Location = new System.Drawing.Point(182, 191);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Location = new System.Drawing.Point(136, 155);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(83, 28);
+            this.radioButton2.Size = new System.Drawing.Size(70, 22);
             this.radioButton2.TabIndex = 33;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "★★★★";
@@ -223,10 +218,9 @@
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton1.Location = new System.Drawing.Point(17, 191);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Location = new System.Drawing.Point(13, 155);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(70, 28);
+            this.radioButton1.Size = new System.Drawing.Size(59, 22);
             this.radioButton1.TabIndex = 32;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "★★★";
@@ -236,20 +230,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(12, 27);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(9, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 24);
+            this.label1.Size = new System.Drawing.Size(98, 18);
             this.label1.TabIndex = 31;
             this.label1.Text = "Şehir Seçiniz:";
             // 
             // checkBox6
             // 
             this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(138, 578);
-            this.checkBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox6.Location = new System.Drawing.Point(104, 470);
             this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(135, 21);
+            this.checkBox6.Size = new System.Drawing.Size(104, 17);
             this.checkBox6.TabIndex = 30;
             this.checkBox6.Text = "Deniz Manzarası";
             this.checkBox6.UseVisualStyleBackColor = true;
@@ -257,10 +249,9 @@
             // checkBox5
             // 
             this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(138, 529);
-            this.checkBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox5.Location = new System.Drawing.Point(104, 430);
             this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(139, 21);
+            this.checkBox5.Size = new System.Drawing.Size(108, 17);
             this.checkBox5.TabIndex = 29;
             this.checkBox5.Text = "Havuz Manzarasi";
             this.checkBox5.UseVisualStyleBackColor = true;
@@ -268,10 +259,9 @@
             // checkBox4
             // 
             this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(138, 479);
-            this.checkBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox4.Location = new System.Drawing.Point(104, 389);
             this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(76, 21);
+            this.checkBox4.Size = new System.Drawing.Size(60, 17);
             this.checkBox4.TabIndex = 28;
             this.checkBox4.Text = "Minibar";
             this.checkBox4.UseVisualStyleBackColor = true;
@@ -279,10 +269,9 @@
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(19, 578);
-            this.checkBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox3.Location = new System.Drawing.Point(14, 470);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(78, 21);
+            this.checkBox3.Size = new System.Drawing.Size(62, 17);
             this.checkBox3.TabIndex = 27;
             this.checkBox3.Text = "İnternet";
             this.checkBox3.UseVisualStyleBackColor = true;
@@ -290,10 +279,9 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(19, 529);
-            this.checkBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox2.Location = new System.Drawing.Point(14, 430);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(48, 21);
+            this.checkBox2.Size = new System.Drawing.Size(40, 17);
             this.checkBox2.TabIndex = 26;
             this.checkBox2.Text = "TV";
             this.checkBox2.UseVisualStyleBackColor = true;
@@ -301,10 +289,9 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(19, 479);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox1.Location = new System.Drawing.Point(14, 389);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(64, 21);
+            this.checkBox1.Size = new System.Drawing.Size(51, 17);
             this.checkBox1.TabIndex = 25;
             this.checkBox1.Text = "Klima";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -319,18 +306,28 @@
             "Bursa",
             "Antalya",
             "Mersin"});
-            this.comboBox1.Location = new System.Drawing.Point(183, 27);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Location = new System.Drawing.Point(137, 22);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(150, 24);
+            this.comboBox1.Size = new System.Drawing.Size(114, 21);
             this.comboBox1.TabIndex = 24;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(493, 155);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(86, 17);
+            this.checkBox10.TabIndex = 48;
+            this.checkBox10.Text = "checkBox10";
+            this.checkBox10.UseVisualStyleBackColor = true;
             // 
             // OtelSecmesi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1213, 625);
+            this.ClientSize = new System.Drawing.Size(910, 508);
+            this.Controls.Add(this.checkBox10);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dateTimePicker1);
@@ -355,6 +352,7 @@
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "OtelSecmesi";
             this.Text = "OtelSecmesi";
             this.ResumeLayout(false);
@@ -388,5 +386,6 @@
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox10;
     }
 }
