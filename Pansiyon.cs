c665bedeca0d9968bea1e20752f483fcde26ee86
berkayadsan <47854329+berkayadsan@users.noreply.github.com>
2019-06-12 17:27:30 +0300

    berkay

---
+++ Pansiyon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje2
+{
+    class Pansiyon:Otel
+    {
+        private string sehir;
+
+       
+
+        public Pansiyon(int otelid,string otelad, int yildiz, bool spa, bool havuz, bool sporsalonu) : base(otelid,otelad,yildiz,spa,havuz,sporsalonu)//otelden gelir
+        {
+            pansiyon = true;
+
+        }
+        
+    }
+}
